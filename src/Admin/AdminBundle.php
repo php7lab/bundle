695606e603610e05092118ce786d80aa678f2dc6
Vitaliy <theyamshikov@yandex.ru>
2020-02-02 22:19:21 +0600

    refactor: move admin bundle

---
+++ src/Admin/AdminBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace PhpLab\Bundle\Admin;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AdminBundle extends Bundle
+{
+
+}
---
+++ src/Admin/Widgets/MenuWidget.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace PhpLab\Bundle\Admin\Widgets;
+
+class MenuWidget extends \PhpLab\Web\Widgets\MenuWidget
+{
+
+    public $itemOptions = [
+        'class' => 'nav-item',
+    ];
+    public $linkTemplate =
+        '<a href="{url}" class="nav-link {class}">
+            {icon}
+            <p>
+                {label}
+                {treeViewIcon}
+                {badge}
+            </p>
+        </a>';
+    public $submenuTemplate = '<ul class="nav nav-treeview">{items}</ul>';
+    public $activateParents = true;
+    public $treeViewIcon = '<i class="right fas fa-angle-left"></i>';
+
+    public function __construct()
+    {
+        $this->items = include(__DIR__ . '/../../../../../../config/menu/admin.php');
+    }
+
+}
