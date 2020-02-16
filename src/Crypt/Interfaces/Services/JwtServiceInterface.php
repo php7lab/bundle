a3a4b1b9566879243b35bff6dc3d8fb213109894
Vitaliy <theyamshikov@yandex.ru>
2020-02-16 15:34:26 +0600

    feat: fill interface

--- src/Crypt/Interfaces/Services/JwtServiceInterface.php
+++ src/Crypt/Interfaces/Services/JwtServiceInterface.php
@@ -2,9 +2,14 @@
 
 namespace PhpLab\Bundle\Crypt\Interfaces\Services;
 
+use PhpLab\Bundle\Crypt\Entities\JwtEntity;
+
 interface JwtServiceInterface
 {
 
+    public function sign(JwtEntity $jwtEntity, string $profileName): string;
+    public function verify(string $token, string $profileName): JwtEntity;
+    public function decode(string $token);
+    public function setProfiles($profiles);
 
 }
-
