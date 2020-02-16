922a226e0b7168e97f04716762211521c82e1fa9
Vitaliy <theyamshikov@yandex.ru>
2020-02-16 13:38:45 +0600

    feat: key profiles to env

--- src/Crypt/Repositories/Config/ProfileRepository.php
+++ src/Crypt/Repositories/Config/ProfileRepository.php
@@ -3,20 +3,24 @@
 namespace PhpLab\Bundle\Crypt\Repositories\Config;
 
 use PhpLab\Bundle\Crypt\Interfaces\Repositories\ProfileRepositoryInterface;
+use PhpLab\Core\Domain\Helpers\EntityHelper;
 use PhpLab\Core\Enums\Measure\TimeEnum;
 use PhpLab\Bundle\Crypt\Entities\JwtProfileEntity;
 use PhpLab\Bundle\Crypt\Entities\KeyEntity;
+use PhpLab\Core\Libs\Env\DotEnvHelper;
 
 class ProfileRepository implements ProfileRepositoryInterface
 {
 
     public function oneByName(string $profileName)
     {
+        $prifile = DotEnvHelper::get('jwt.profiles.' . $profileName);
+        $keyEntity = new KeyEntity;
+        EntityHelper::setAttributes($keyEntity, $prifile['key']);
         $profileEntity = new JwtProfileEntity;
         $profileEntity->name = $profileName;
-        $profileEntity->key = new KeyEntity;
-        $profileEntity->key->private = 'W4PpvVwI82Rfl9fl2R9XeRqBI0VFBHP3';
-        $profileEntity->life_time = TimeEnum::SECOND_PER_YEAR;
+        $profileEntity->key = $keyEntity;
+        $profileEntity->life_time = $prifile['life_time'] ?? TimeEnum::SECOND_PER_YEAR;
         return $profileEntity;
     }
 
